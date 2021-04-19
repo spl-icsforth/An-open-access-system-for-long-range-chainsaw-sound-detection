9fb6c3f352f2d42155cd390c101fe963df249048
konpsar <psaroulakis.kon@gmail.com>
2021-04-19 13:08:22 +0300

    Added probability threshold AND number of cores selection frames
    Added frames with the same format (slider + text entry) for probability threshold selection and number of cpu cores to use

--- detect_chainsaw_gui.py
+++ detect_chainsaw_gui.py
@@ -50,6 +50,31 @@ class MainApplication(tk.Frame):
         vad_slider.grid(row=0, column=2, padx=(10), pady=10, sticky='NSEW')
 
 
+        prob_frame = tk.Frame(parent)
+        prob_frame.grid(row=3, column=0, columnspan=3, padx=10, pady=10, sticky=E+W+N+S)
+        prob_lbl = tk.Label(prob_frame, text = f"Probability threshold chosen: \n[Can take value between 0-1]")
+        prob_lbl.grid(row=0, column=0, padx=(10), pady=10,sticky=E+W+N+S)
+        prob_th = tk.DoubleVar()
+        prob_th.set(0.75)
+        prob_textbox = tk.Entry(prob_frame, textvariable=prob_th)
+        prob_textbox.grid(row=0, column=1, padx=(10), pady=10)
+        prob_slider = tk.Scale(prob_frame, 
+                    from_=0, to=1, resolution = 0.01, 
+                    orient=tk.HORIZONTAL, variable = prob_th)
+        prob_slider.grid(row=0, column=2, padx=(10), pady=10)
+
+        cpu_frame = tk.Frame(parent)
+        cpu_frame.grid(row=4, column=0, columnspan=3, padx=10, pady=10, sticky=E+W+N+S)
+        cpu_lbl = tk.Label(cpu_frame, text = f"Number of CPU units employed:")
+        cpu_lbl.grid(row=0, column=0, padx=(10), pady=10)
+        cpus = tk.IntVar()
+        cpus.set(6)
+        cpu_textbox = tk.Entry(cpu_frame, textvariable=cpus)
+        cpu_textbox.grid(row=0, column=1, padx=(10), pady=10)
+        cpu_slider = tk.Scale(cpu_frame, 
+                    from_=1,to=16, resolution = 1, 
+                    orient=tk.HORIZONTAL, variable = cpus)
+        cpu_slider.grid(row=0, column=2, padx=(10), pady=10)
 
 
 
